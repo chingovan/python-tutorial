d193175290f808bcd97e326b115a551567209f8b
programmerit <chingovan@gmail.com>
2015-06-03 17:05:50 +0700

    c

---
+++ SpecialNumberFunction/src/primenumber.py
@@ -0,0 +1,36 @@
+"""
+Problem:     Day la mot vi du don gian, nhap vao mot so nguyen va kiem tra xem co phai la so nguyen to hay khong.
+            So nguyen to la cac so nguyen duong chi chia het cho 1 va chinh no.
+Coder:       Chi Ngo (chingovan@gmail.com)
+Date:        2015/06/03
+Slogan:      For a brighter future!
+"""
+def isPrimerNumber(num):
+    
+    if num < 2:
+        
+        return False
+    elif num == 2:
+     
+        return True
+    else:
+        isPrimeNumber = True;
+        for i in range(2, num):
+            if num % i == 0:
+                isPrimeNumber = False;
+                break
+        
+        if isPrimeNumber == True:
+            return True
+        else:
+            return  False
+
+number = int(input("Nhap vao mot so nguyen: "))
+
+result = isPrimerNumber(number)
+if result == True:
+    
+    print(number, " la so nguyen to")
+else:
+    
+    print(number, " khong la so nguyen to")
